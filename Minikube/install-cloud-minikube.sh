58ca962498cabd0c2f84e35f038fabba60f18e7e
VictorSandoval <53907515+VictorGSandoval@users.noreply.github.com>
2022-08-25 00:17:16 -0500

    Update install-cloud-minikube.sh

--- Minikube/install-cloud-minikube.sh
+++ Minikube/install-cloud-minikube.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 echo "Install dependencies"
-sudo su
-apt update -y
-apt install -y curl wget apt-transport-https ca-certificates gnupg lsb-release conntrack
+
+sudo apt update -y
+sudo apt install -y curl wget apt-transport-https ca-certificates gnupg lsb-release conntrack
 
 echo "Install docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt install docker-ce docker-ce-cli containerd.io -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 echo "Docker user root"
-usermod -aG docker victor_sandoval
-systemctl restart docker
-newgrp docker
+sudo usermod -aG docker $USER
+sudo newgrp docker
 
 echo "Install Kubectl"
+sudo -i
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 mv kubectl /bin/kubectl
 chmod a+x /bin/kubectl
