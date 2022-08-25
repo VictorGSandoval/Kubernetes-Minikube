80cf7822da89f53342c15011e5bdfd6e033af0a2
VictorSandoval <53907515+VictorGSandoval@users.noreply.github.com>
2022-08-25 18:50:22 -0500

    Update installcri.sh

--- Minikube/installcri.sh
+++ Minikube/installcri.sh
@@ -5,6 +5,6 @@ git clone https://github.com/Mirantis/cri-dockerd.git
 wget https://storage.googleapis.com/golang/getgo/installer_linux
 chmod +x ./installer_linux
 ./installer_linux
-source ~/.bash_profile
+source /etc/profile
 
 echo "Finish"
