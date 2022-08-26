399db4e7696cbc14c17c60500ed2fb77ff0f58f0
VictorSandoval <53907515+VictorGSandoval@users.noreply.github.com>
2022-08-26 00:18:58 -0500

    Update installcri.sh

--- Minikube/installcri.sh
+++ Minikube/installcri.sh
@@ -2,6 +2,7 @@
 sudo -i
 echo "Inicio"
 sudo curl -Lo go_installer https://get.golang.org/$(uname) && chmod +x go_installer && ./go_installer && rm go_installer
+source $HOME/.profile
 go version
 
 echo "Finish"
